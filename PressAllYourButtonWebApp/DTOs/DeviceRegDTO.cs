a625a19a8f2b05d753ef90a8ba55c331cdd9efef
unknown <pqcamh228@gmail.com>
2022-01-31 16:49:49 +0800

    20220131 AddRegDeviceFunction

---
+++ PressAllYourButtonWebApp/DTOs/DeviceRegDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace PressAllYourButtonWebApp.DTOs
+{
+    public class DeviceRegDTO
+    {
+        public Guid Id { get; set; }
+
+        public int userId { get; set; }
+        public string? NicknameByUser { get; set; }
+    }
+}
--- PressAllYourButtonWebApp/Services/DeviceService.cs
+++ PressAllYourButtonWebApp/Services/DeviceService.cs
@@ -1,6 +1,36 @@
-﻿namespace PressAllYourButtonWebApp.Services
+﻿using PressAllYourButtonWebApp.DTOs;
+using PressAllYourButtonWebApp.Models;
+
+namespace PressAllYourButtonWebApp.Services
 {
     public class DeviceService
     {
+        PressAYBDbContext dbContext;
+        public DeviceService(PressAYBDbContext db)
+        {
+            dbContext = db;
+        }
+
+        public async Task<string> RegDevice(DeviceRegDTO value)
+        {
+            var device = dbContext.Devices.Where(d => d.Id == value.Id).SingleOrDefault();
+            if (device == null)
+                return "Device Not Exists";
+            if (device.Belong_User != null)
+                return "Device has been registered";
+
+            device.Belong_User = value.userId;
+            device.NicknameByUser = value.NicknameByUser;
+
+            await dbContext.SaveChangesAsync();
+
+            string log = string.Format("Device {0} Has been added", value.NicknameByUser);
+
+
+            return log;
+        }
+
+
+
     }
 }
